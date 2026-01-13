1a452f72f669407b5c4d9837acdadba8bcad0e8a
Andrei Dubrovin <wildsnowcat7@gmail.com>
2026-01-13 10:31:10 +0300

    streamlit-pablic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-st.title("Калькулятор")
+st.title("Congratulations!!")
 
-val_1 = st.number_input("Insert number 1:", key="val_1")
-val_2 = st.number_input("Insert number 2:", key="val_2")
+# val_1 = st.number_input("Insert number 1:", key="val_1")
+# val_2 = st.number_input("Insert number 2:", key="val_2")
 
-st.write(f"Результат = {val_1 + val_2}")
+# st.write(f"Результат = {val_1 + val_2}")
 
+first_name = st.text_input("Input first_name:", key="first_name")
+last_name = st.text_input("Input last_name:", key="last_name")
+
+st.write(f"Hello, {first_name} {last_name}!")
